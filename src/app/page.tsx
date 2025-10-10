c1d741b3c4a624608738097fc0334b4775723e60
lucas lacerda <desenvolverbg@gmail.com>
2025-10-10 19:08:02 -0300

    feat: add button component and update project configuration files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@components/Button";
 
 export default function Home() {
   return (
+    <div>
       <h1>Entre com sua conta spotify clicando no botão abaixo</h1>
-  )
+      <Button>Testando</Button>
+    </div>
+  );
 }
