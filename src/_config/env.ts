a179e78fecbc42cf80ab735f99170822319f3c67
lucas lacerda <desenvolverbg@gmail.com>
2025-10-16 10:53:19 -0300

    refactor: config prisma instead of sqlite

--- src/_config/env.ts
+++ src/_config/env.ts
@@ -1,9 +1,10 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { betterAuthEnv } from "@/_config/better-auth.env";
+import {vercel} from "@t3-oss/env-core/presets-zod";
+import {createEnv} from "@t3-oss/env-nextjs";
+import {betterAuthEnv} from "@/_config/better-auth.env";
 
 export const env = createEnv({
   server: {},
   experimental__runtimeEnv: process.env,
-  extends: [betterAuthEnv],
+  extends: [betterAuthEnv, vercel()],
   isServer: typeof window === "undefined",
 });
--- src/auth.ts
+++ src/auth.ts
@@ -5,13 +5,23 @@ import {nextCookies} from "better-auth/next-js";
 import {env} from "@/_config/env";
 
 const prisma = new PrismaClient();
+const trustedOrigin = ["http://127.0.0.1:3000"];
+
+if (env.VERCEL_BRANCH_URL) {
+  trustedOrigin.push(env.VERCEL_BRANCH_URL);
+}
+
+if (env.VERCEL_URL) {
+  trustedOrigin.push(env.VERCEL_URL);
+}
+
 export const auth = betterAuth({
   plugins: [nextCookies()],
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   baseURL: "http://127.0.0.1:3000",
-  trustedOrigins: ["http://127.0.0.1:3000"],
+  trustedOrigins: trustedOrigin,
   advanced: {
     cookiePrefix: "better-auth",
     useSecureCookies: false,
