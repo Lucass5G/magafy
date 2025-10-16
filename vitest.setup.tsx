f9ec88bbd478f2de02177e5ccded48e88c346cb6
lucas lacerda <desenvolverbg@gmail.com>
2025-10-16 14:49:19 -0300

    test(Playlists): add new tests to playlists

--- vitest.setup.tsx
+++ vitest.setup.tsx
@@ -2,10 +2,8 @@ import "@testing-library/jest-dom";
 import {vi} from "vitest";
 
 vi.mock("next/image", () => ({
-  default: (props: unknown) => {
-    // next/image props are spread onto the img tag
-    // eslint-disable-next-line @next/next/no-img-element
-    // @ts-expect-error
-    return <img {...props} alt={"mock"} />;
+  default: (props: any) => {
+    // biome-ignore lint/a11y/useAltText: <explanation>
+    return <img {...props} />;
   },
 }));
